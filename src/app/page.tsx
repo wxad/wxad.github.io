e28d8b1ce8aa967a6f67ab0e74dfef19c37bcde9
yijiejiang <yijiejiang@tencent.com>
2025-04-15 23:43:46 +0800

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,6 +102,25 @@ const ships = [
     link: "https://wxad.design/adui-mob",
   },
   {
+    name: "Spross",
+    desc: "灵活、好用、没有问题的浮层组件",
+    logo: (
+      <svg viewBox="0 0 24 24">
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        >
+          <path d="M7 20h10m-7 0c5.5-2.5.8-6.4 3-10" />
+          <path d="M9.5 9.4c1.1.8 1.8 2.2 2.3 3.7c-2 .4-3.5.4-4.8-.3c-1.2-.6-2.3-1.9-3-4.2c2.8-.5 4.4 0 5.5.8M14.1 6a7 7 0 0 0-1.1 4c1.9-.1 3.3-.6 4.3-1.4c1-1 1.6-2.3 1.7-4.6c-2.7.1-4 1-4.9 2" />
+        </g>
+      </svg>
+    ),
+    link: "https://wxad.design/spross/",
+  },
+  {
     name: "React Squircle",
     desc: "React 曲率连续圆角组件",
     logo: (
